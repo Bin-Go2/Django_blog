e56b5057078c05fa57e458b9b0c2c57cccc96221
SondersB <binzhangsam@gmail.com>
2020-06-07 17:00:54 -0700

    day2

--- my_blog/article/urls.py
+++ my_blog/article/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
-app_name = 'article'
+
+from . import views
+
+app_name = 'article1'
+
 urlpatterns = [
-    # 目前还没有urls
+
+    path('article-list/', views.article_list, name='article_list'),
+      # 文章详情
+    path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
 ]
